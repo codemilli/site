5e5950c4c7848b0fbf11080a6cfe6f0ad2077715
Jooyoung Moon <hckrmoon@gmail.com>
2017-03-09 16:50:17 +0900

    code changes

--- og-debug/public/js/submit.js
+++ og-debug/public/js/submit.js
@@ -1,15 +1,24 @@
 $(function () {
+  var ogMetaList = ['og:type', 'og:url', 'og:image', 'og:image:width', 'og:image:height', 'og:title', 'og:description', 'fb:app_id'];
+
   $('form#debug').on('submit', function (e) {
     e.preventDefault();
 
     var $target = $("#target");
-    var url = $target.val();
+    var $iframe = $('#iframe');
+    var url     = $target.val();
 
-    $.ajax(url).then(function (response) {
-      var $headTag = $("<head>" + response.split('<head>')[1].split('</head>')[0] + "</head>");
+    $iframe.attr('src', url);
+    $iframe.load(function () {
+      var metas = $iframe.contents().find('meta');
 
-      console.log('response', $headTag);
-      window.$headTag = $headTag;
+      $.each(metas, function (idx) {
+        var meta    = metas[idx];
+        var name    = meta.name;
+        var content = meta.content;
+
+        console.log('meta', name, content);
+      });
     });
   });
 });
