419522b55debdab04e5abf568f3519132fe32a4c
Jooyoung Moon <hckrmoon@gmail.com>
2017-03-14 10:15:40 +0900

    ui and js updated

--- og-debug/public/js/submit.js
+++ og-debug/public/js/submit.js
@@ -1,25 +1,28 @@
 $(function () {
   var ogMetaList = ['og:type', 'og:url', 'og:image', 'og:image:width', 'og:image:height', 'og:title', 'og:description', 'fb:app_id'];
   var metaTags   = [];
+  var domList    = [];
+  var url        = "";
+  var imageUrl   = "";
+  var ogUrl      = "";
 
   $('form#debug').on('submit', function (e) {
     e.preventDefault();
 
     var $target = $("#target");
     var $iframe = $('#iframe');
-    var url     = $target.val();
 
-    $iframe.attr('src', url);
-    $iframe.load(function () {
-      var metas;
+    url         = $target.val();
 
-      try {
-        metas = $iframe.contents().find('meta');
-      } catch (e) {
-        console.log('e', e);
-      }
+    $.ajax({
+      method  : 'get',
+      url     : url
+    }).then(function (response) {
+      $("#hidden__div").append($.parseHTML(response));
 
-      $.each(metas, function (idx) {
+      var metas = $("#hidden__div").find("meta") || [];
+
+      $.each(metas, function(idx) {
         var meta    = metas[idx];
         var name    = meta.name;
         var content = meta.content;
@@ -29,11 +32,28 @@ $(function () {
           obj.name        = name;
           obj.content     = content;
           metaTags.push(obj);
+
+          if (name === "og:image") {
+            imageUrl = content;
+          }
+          if (name === "og:url") {
+            ogUrl = content;
+          }
         }
       });
 
+      $.each(metaTags, function (idx) {
+        var meta = metaTags[idx];
+        domList.push("<tr> <td class='result__body--title'>"+meta.name+"</td> <td class='result__body--content'>"+meta.content+"</td> </tr>");
+      });
 
-      console.log('ogMetaMap', metaTags);
+      console.log('domList', domList);
+
+      $.each(domList, function (idx) {
+        var dom = domList[idx];
+        $(".result__content tbody").append(dom);
+        console.log('dom', dom);
+      });
     });
   });
 });
