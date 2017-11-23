495a51cfb538849205ca61b364fc8305b5d89a61
Jooyoung Moon <hckrmoon@ncsoft.com>
2017-11-23 17:30:46 +0900

    configs

--- smart-toon/main.js
+++ smart-toon/main.js
@@ -2,7 +2,24 @@
  * 2017. 11. 23.
  */
 
-var viewer = $('.viewer')
+$.get('/smart-toon/data/mock.json')
+ .then(calculateViewerSize)
+ .then(onData)
+
+function calculateViewerSize(res) {
+  const viewer = $('.viewer'),
+      width = viewer.width(),
+      height = viewer.height()
+
+  return {
+    viewer: {width, height},
+    toon: res
+  }
+}
+
+function onData({viewer, toon}) {
+  const ratio = viewer.width / toon.background.width
+}
 
 
 
