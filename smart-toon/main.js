4044b60c731a13408eb541e33a6a2c15dca4a1f3
Jooyoung Moon <hckrmoon@ncsoft.com>
2017-11-23 18:16:28 +0900

    priority added

--- smart-toon/main.js
+++ smart-toon/main.js
@@ -2,14 +2,20 @@
  * 2017. 11. 23.
  */
 
-$.get('/smart-toon/data/mock.json')
- .then(calculateViewerSize)
- .then(onData)
+const $viewer = $('.viewer')
+
+onLoad()
+  .then(() => $.get('/smart-toon/data/mock.json'))
+  .then(calculateViewerSize)
+  .then(onData)
+
+function onLoad() {
+  return new Promise((resolve) => $(window).load(resolve))
+}
 
 function calculateViewerSize(res) {
-  const viewer = $('.viewer'),
-      width = viewer.width(),
-      height = viewer.height()
+  const width = $viewer.width(),
+        height = $viewer.height()
 
   return {
     viewer: {width, height},
@@ -19,6 +25,22 @@ function calculateViewerSize(res) {
 
 function onData({viewer, toon}) {
   const ratio = viewer.width / toon.background.width
+
+  console.log('toon : ', toon)
+  console.log('ratio : ', ratio)
+  $viewer.css({"height": `${toon.background.height * ratio}px`})
+  $viewer.find('#bg-image').attr('src', toon.background.image_url)
+
+  _.map(toon.image_objects, (data, i) => {
+    const imgObj = ImageObjectTmpl({
+      imageUrl: data.image_url,
+      priority: data.priority
+    })
+
+    console.log('imgObj', imgObj)
+
+    $viewer.append(imgObj)
+  })
 }
 
 
--- smart-toon/template/image-object.js
+++ smart-toon/template/image-object.js
@@ -3,7 +3,8 @@
  */
 
 var ImageObjectTmpl = _.template(`
-  <div class="image-object">
-    <img src="<%= imageUrl %>" alt="image object" />
+  <div class="image-object" 
+       style="z-index: <%= priority %>;">
+    <img style="" src="<%= imageUrl %>" alt="image object" />
   </div>
 `)
