1b2b30b7e0d3e27809d68760a75102877ec64988
Jooyoung Moon <hckrmoon@gmail.com>
2018-04-25 09:41:41 +0900

    updates

--- storypage.9d488d0285b7102ec38c.js
+++
@@ -1,5 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["storypage"],{"./src/pages/StoryPage/StoryPage.tsx":
-/*!*******************************************!*\
-  !*** ./src/pages/StoryPage/StoryPage.tsx ***!
-  \*******************************************/
-/*! exports provided: StoryPage */function(e,t,n){"use strict";n.r(t),n.d(t,"StoryPage",function(){return u});var r=n(/*! tslib */"./node_modules/tslib/tslib.es6.js"),o=n(/*! react */"./node_modules/react/index.js"),s=n.n(o),a=n(/*! ../../core/utils/Number */"./src/core/utils/Number.ts"),c=n(/*! ../../components/Header/Header */"./src/components/Header/Header.tsx"),u=function(e){function t(t){return e.call(this,t)||this}return r.__extends(t,e),t.prototype.render=function(){return s.a.createElement("div",null,s.a.createElement(c.Header,null),s.a.createElement("h2",null," Story Page !!!!! ",Object(a.toNumber)("1")," "))},t}(s.a.Component)}}]);
